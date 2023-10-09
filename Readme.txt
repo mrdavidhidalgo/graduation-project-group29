52a228cb627ae3c6f790720fa9f552c2b6fcc877
David Hidalgo <d.hidalgo@uniandes.edu.co>
2023-10-08 23:29:01 -0500

    redeploy

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+# Proyecto Comandos para iniciar servicio
+gcloud run services replace service.yml
+gcloud run services set-iam-policy backend  policy.yaml
--- controllers/health_rest_controller.py
+++ controllers/health_rest_controller.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
 router = APIRouter()
-
+from services import logs
+LOGGER = logs.get_logger()
 @router.get("/health", status_code=200)
 async def health():
-    
+    LOGGER.info("health")
     return {"msg": "Up"}
