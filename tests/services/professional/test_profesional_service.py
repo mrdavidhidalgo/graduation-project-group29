a146f1b53ed382784ea3784a32f05d3c79fdd924
David Hidalgo <d.hidalgo@uniandes.edu.co>
2023-10-15 23:42:11 -0500

    unit tests

---
+++ tests/services/professional/test_profesional_service.py
@@ -0,0 +1,8 @@
+from typing import Optional
+
+import pytest
+
+@pytest.mark.unittests
+def test_should_raise_exception_trying_to_create_professionl_but_it_already_exist()->None:
+    
+    print("OK")
